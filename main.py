37db7a68b59a8968097dd57ef7fd4a8ae49af3f7
Derick Soares <dspsoares@outlook.com>
2025-02-21 10:50:11 -0300

    refactor command responses in chatbot to streamline user guidance and update webhook route

--- main.py
+++ main.py
@@ -10,9 +10,7 @@ app = Flask(__name__)
 
 # Define responses for specific commands
 RESPONSES = {
-    '!help': 'Olá, bem vindo ao atendimento Inovar!',
-    '!options': 'Escolha uma das opções abaixo:\n\n 1 - Option 1\n 2 - Option 2\n 3 - Option 3',
-    '!contact': 'Entre em contato conosco pelo telefone (19) 9 9999-9999 ou pelo e-mail contato@inovar.com.br',
+    'default': 'Bem vindo ao atendimento da Inovar! \n\nPara seguirmos com seu atendimento, escolha uma das opções abaixo: \n\n1 - Opção 1 \n2 - Opção 2 \n3 - Opção 3',
     '1': 'Você escolheu a opção 1.',
     '2': 'Você escolheu a opção 2.',
     '3': 'Você escolheu a opção 3.',
@@ -20,6 +18,7 @@ RESPONSES = {
 
 IMAGE_PATH = './files/helicopter.jfif'
 IMAGE_CAPTION = 'Caption.'
+
 
 def send_whapi_request(endpoint, payload):
     """Send a request to the WhatsApp API."""
@@ -32,13 +31,9 @@ def send_whapi_request(endpoint, payload):
 
     # Check if we're sending an image
     if 'media' in payload:
-
-        # Split the media path and MIME type for image
         image_path, mime_type = payload.pop('media').split(';')
-        
-        with open(image_path, 'rb') as image_file:
 
-            # Create a MultipartEncoder for the file upload
+        with open(image_path, 'rb') as image_file:
             m = MultipartEncoder(
                 fields={
                     **payload,
@@ -49,20 +44,26 @@ def send_whapi_request(endpoint, payload):
             headers['Content-Type'] = m.content_type
             response = requests.post(url, data=m, headers=headers)
     else:
-        # For text messages, use JSON encoding
         headers['Content-Type'] = 'application/json'
         response = requests.post(url, json=payload, headers=headers)
-    
+
+    # Log the request and response for debugging
+    # print(f"Request URL: {url}")
+    # print(f"Request Headers: {headers}")
+    # print(f"Request Payload: {payload}")
+    # print(f"Response Status Code: {response.status_code}")
+    # print(f"Response Content: {response.content}")
+
     return response.json()
 
 
 # The Webhook link to your server is set in the dashboard.
 # For this script it is important that the link is in the format: {link to server}/hook.
-@app.route('/hook', methods=['POST'])
+@app.route('/webhook', methods=['POST'])
 def handle_new_messages():
     try:
         messages = request.json.get('messages', [])
-        
+
         for message in messages:
             # Ignore messages from the bot itself
             if message.get('from_me'):
@@ -74,24 +75,15 @@ def handle_new_messages():
 
             if command_type == 'text':
                 # Get the command text from the incoming message
-                command_text = message.get('text', {}).get('body', '').strip().lower()
-                
+                command_text = message.get('text', {}).get(
+                    'body', '').strip().lower()
+
                 # Determine the response based on the command
-                if command_text == '!help':
-                    payload['body'] = RESPONSES['!help']
-                    endpoint = 'messages/text'
-                elif command_text == '!options':
-                    payload['body'] = RESPONSES['!options']
-                    endpoint = 'messages/text'
-                elif command_text == '!contact':
-                    payload['body'] = RESPONSES['!contact']
-                    endpoint = 'messages/text'
-                elif command_text in RESPONSES:
+                if command_text in RESPONSES:
                     payload['body'] = RESPONSES[command_text]
-                    endpoint = 'messages/text'
                 else:
-                    payload['body'] = "Desculpe, não entendi o comando.\n Digite !options para ver os comandos disponíveis."
-                    endpoint = 'messages/text'
+                    payload['body'] = RESPONSES['default']
+                endpoint = 'messages/text'
 
             elif command_type == 'image':
                 payload['caption'] = IMAGE_CAPTION
@@ -102,7 +94,7 @@ def handle_new_messages():
             send_whapi_request(endpoint, payload)
 
         return 'Ok', 200
-    
+
     except Exception as e:
         print(f"Error: {e}")
         return str(e), 500
